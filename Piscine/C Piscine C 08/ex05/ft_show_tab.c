fcc43118129f2800fdcfc3fe74f1684e43e9931c
João Oliveira <38820667+jotavare@users.noreply.github.com>
2026-09-15 00:25:43 +0100

    fix: null-terminate ft_strdup and free the stock_str tab
    ft_strdup in both ex04 and ex05 never wrote the trailing '\0', so any
    consumer that walked the copy past its source length read garbage,
    and neither malloc call was checked for failure. ex05's main also
    never freed what ft_strs_to_tab allocated, leaking every copy plus
    the array itself on each run.

--- Piscine/C Piscine C 08/ex05/ft_show_tab.c
+++ Piscine/C Piscine C 08/ex05/ft_show_tab.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include "ft_stock_str.h"
 
 struct s_stock_str *ft_strs_to_tab(int ac, char **av);
@@ -72,8 +73,16 @@ void ft_show_tab(struct s_stock_str *par)
 int main(int argc, char **argv)
 {
 	struct s_stock_str *i;
+	int j;
 
 	i = ft_strs_to_tab(argc, argv);
 	ft_show_tab(i);
+	j = 0;
+	while (j < argc)
+	{
+		free(i[j].copy);
+		j++;
+	}
+	free(i);
 	return (0);
 }
--- Piscine/C Piscine C 08/ex05/ft_strs_to_tab.c
+++ Piscine/C Piscine C 08/ex05/ft_strs_to_tab.c
@@ -32,12 +32,15 @@ char	*ft_strdup(char *src)
 	while (src[i])
 		i++;
 	dest = (char *) malloc(i * sizeof(char) + 1);
+	if (!dest)
+		return (0);
 	i = 0;
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
 
